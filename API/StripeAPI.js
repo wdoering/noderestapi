095d5fdb2d2fe1950e6631c1fa4fe0daf6a34835
Wagner Doering <wagner.doering@criticalsoftware.com>
2019-03-11 10:20:32 +0000

    here goes nothing

--- API/StripeAPI.js
+++ API/StripeAPI.js
@@ -3,6 +3,11 @@ var querystring = require('querystring');
 const API_KEY = 'pk_test_2avaANL04219SY9rpZA4U8Rm';
 const API_ENDPOINT = 'https://api.stripe.com/v1/charges';
 
+var stripeAPI = {};
+
+
+
+// suposto step 2
 /**
  *
  *
@@ -15,65 +20,61 @@ const API_ENDPOINT = 'https://api.stripe.com/v1/charges';
   });
 })();
  */
-var stripeAPI = {};
 
-
-
-
-stripeAPI.sendTwilioSms = function (phone, msg, callback) {
+stripeAPI.requestPaymentToken = function (phone, msg, callback) {
     // Validate parameters
-    phone = typeof (phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false;
-    msg = typeof (msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg.trim() : false;
-    if (phone && msg) {
+    // phone = typeof (phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false;
+    // msg = typeof (msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg.trim() : false;
+    // if (phone && msg) {
 
-        // Configure the request payload
-        var payload = {
-            'From': config.twilio.fromPhone,
-            'To': '+1' + phone,
-            'Body': msg
-        };
-        var stringPayload = querystring.stringify(payload);
+    // Configure the request payload
+    var payload = {
+        'From': config.twilio.fromPhone,
+        'To': '+1' + phone,
+        'Body': msg
+    };
+    var stringPayload = querystring.stringify(payload);
 
 
-        // Configure the request details
-        var requestDetails = {
-            'protocol': 'https:',
-            'hostname': 'api.twilio.com',
-            'method': 'POST',
-            'path': '/2010-04-01/Accounts/' + config.twilio.accountSid + '/Messages.json',
-            'auth': config.twilio.accountSid + ':' + config.twilio.authToken,
-            'headers': {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': Buffer.byteLength(stringPayload)
-            }
-        };
+    // Configure the request details
+    var requestDetails = {
+        'protocol': 'https:',
+        'hostname': 'api.twilio.com',
+        'method': 'POST',
+        'path': '/2010-04-01/Accounts/' + config.twilio.accountSid + '/Messages.json',
+        'auth': config.twilio.accountSid + ':' + config.twilio.authToken,
+        'headers': {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(stringPayload)
+        }
+    };
 
-        // Instantiate the request object
-        var req = https.request(requestDetails, function (res) {
-            // Grab the status of the sent request
-            var status = res.statusCode;
-            // Callback successfully if the request went through
-            if (status == 200 || status == 201) {
-                callback(false);
-            } else {
-                callback('Status code returned was ' + status);
-            }
-        });
+    // Instantiate the request object
+    var req = https.request(requestDetails, function (res) {
+        // Grab the status of the sent request
+        var status = res.statusCode;
+        // Callback successfully if the request went through
+        if (status == 200 || status == 201) {
+            callback(false);
+        } else {
+            callback('Status code returned was ' + status);
+        }
+    });
 
-        // Bind to the error event so it doesn't get thrown
-        req.on('error', function (e) {
-            callback(e);
-        });
+    // Bind to the error event so it doesn't get thrown
+    req.on('error', function (e) {
+        callback(e);
+    });
 
-        // Add the payload
-        req.write(stringPayload);
+    // Add the payload
+    req.write(stringPayload);
 
-        // End the request
-        req.end();
+    // End the request
+    req.end();
 
-    } else {
-        callback('Given parameters were missing or invalid');
-    }
+    // } else {
+    //     callback('Given parameters were missing or invalid');
+    // }
 };
 
 
